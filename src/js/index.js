9c036b101c5df3e1e271d82bba876d986096d92d
touza <touzaelhassan@gmail.com>
2022-04-12 17:38:31 +0000

    Teach me school project setup

---
+++ src/js/index.js
@@ -0,0 +1,4 @@
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import 'bootstrap/dist/css/bootstrap.rtl.min.css';
+import 'normalize.css/normalize.css';
+import '../sass/style.scss';
---
+++ webpack.config.js
@@ -0,0 +1,95 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const HtmlWebpackPartialsPlugin = require('html-webpack-partials-plugin');
+
+module.exports = {
+  entry: {
+    'js/main.js': './src/js/index.js',
+  },
+
+  output: {
+    publicPath: '/',
+    path: path.resolve(__dirname, 'build'),
+    filename: '[name].js',
+    clean: true,
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.(sass|css|scss)$/,
+        use: [
+          // Creates `style` nodes from JS strings
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: { publicPath: '../' },
+          },
+          // Translates CSS into CommonJS
+          'css-loader',
+          // Translate new CSS to old CSS
+          'postcss-loader',
+          // Compiles Sass to CSS
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.(png|jpe?g|svg|webp|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'images',
+            },
+          },
+        ],
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader',
+            options: {
+              minimize: true,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+    ],
+  },
+
+  devServer: {
+    port: 9000,
+    open: true,
+    static: {
+      directory: path.join(__dirname, 'build'),
+    },
+    devMiddleware: {
+      writeToDisk: true,
+    },
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: 'index.html',
+      chunks: ['js/main.js'],
+    }),
+
+    new MiniCssExtractPlugin({ filename: 'css/style.css' }),
+
+    new CssMinimizerPlugin(),
+  ],
+};
